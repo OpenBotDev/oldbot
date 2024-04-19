726683cc3640b6ebb6e48386e826246a741a4a44
Ben <benjyc@proton.me>
2024-04-19 17:06:15 +0200

    install sh

--- install.sh
+++ install.sh
@@ -1,29 +1,12 @@
-# openbot
-
-Sniperbot on Solana
-
-Roadmap
-
-* improve time of buy from launch
-* improve buy/sell round trip time
-* advanced selling condition Trailing/Stop
-* reliable
-* analytics on server
-* web frontend
-
-## install
-
-```
-sudo apt update -y
+#!/bin/bash
+sudo apt install curl unzip -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update -y
 sudo apt install yarn -y
 git clone git@github.com:OpenBotDev/sniperbot.git && cd sniperbot
 yarn install
-cp .env.copy .env
-```
 
-## run
 
-```yarn run bot```
